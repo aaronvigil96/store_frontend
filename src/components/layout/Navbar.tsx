0f6264ddef974c8ba1d7a07064324c59dd3d50d0
aaronvigil96 <aaronvigil96@gmail.com>
2025-02-06 14:08:41 -0300

    add in navbar search products

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,4 @@
+import { FiSearch } from "react-icons/fi";
 import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
@@ -6,6 +7,12 @@ const Navbar = () => {
             <Link to={'/'}>
                 <h2 className="text-three text-3xl font-black">3 Digitos</h2>
             </Link>
+            <label className="relative" htmlFor="search">
+                <div className="absolute top-1 left-1">
+                    <FiSearch />
+                </div>
+                <input type="text" className="rounded-sm pl-6 outline-none border border-gray-500 bg-gray-300 focus:border-blue-400" placeholder="azucar" id="search"/>
+            </label>
             <nav className="flex gap-2">
                 <NavLink to={'/auth/login'} className={({isActive}) => ` btn btn-primary ${isActive ? "active-link" : ""}`}>
                     iniciar sesión
